aa65781bed17ba90b73d67c1d2572802875f8791
Brian Ritz <brianritz@Brians-MacBook-Air.local>
2017-09-26 16:22:14 -0400

    made an pip installable package

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+setup(name="brutils", 
+        version="0.1",
+        description="Common utilities for data science",
+        url="https://github.com/bmritz/brutils",
+        author="Brian Ritz",
+        author_email="bmritz@indiana.edu",
+        license="",
+        install_requires = [
+        'gitpython',
+        'pandas',
+        'numpy',
+        'sklearn',
+        'imblearn',
+        'matplotlib',
+        'seaborn',
+        'ipywidgets',
+        'Ipython',
+        'traitlets'
+        ],
+        zip_safe=False
+)

cb2fe94f02741be202206511c8bf1ad4212fb299
Brian Ritz <britz@brianr-mac.dhcp.nd.edu>
2017-09-29 12:11:00 -0400

    added date serializer

--- brutils/jsonutils/serializers.py
+++ brutils/jsonutils/serializers.py
@@ -1,6 +1,9 @@
 """ JSON serializers for use with the python builtin json module
 """
 import json
+import numpy
+
+from datetime import date, datetime
 
 class NumpyEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -11,4 +14,13 @@ class NumpyEncoder(json.JSONEncoder):
         elif isinstance(obj, numpy.ndarray):
             return obj.tolist()
         else:
-            return super(MyEncoder, self).default(obj)
+            return super(NumpyEncoder, self).default(obj)
+
+class DateEncoder(json.JSONEncoder):
+	def default(self, obj):
+    if isinstance(obj, (datetime, date)):
+        return obj.isoformat()
+    else:
+    	return super(DateEncoder, self).default(obj)
+
+

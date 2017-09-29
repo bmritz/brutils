b9e19c54d4548ccff73a660756ef980b2b004d64
Brian Ritz <britz@brianr-mac.dhcp.nd.edu>
2017-09-29 11:49:47 -0400

    initial commit of json utils

---
+++ brutils/jsonutils/serializers.py
@@ -0,0 +1,14 @@
+""" JSON serializers for use with the python builtin json module
+"""
+import json
+
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, numpy.integer):
+            return int(obj)
+        elif isinstance(obj, numpy.floating):
+            return float(obj)
+        elif isinstance(obj, numpy.ndarray):
+            return obj.tolist()
+        else:
+            return super(MyEncoder, self).default(obj)
